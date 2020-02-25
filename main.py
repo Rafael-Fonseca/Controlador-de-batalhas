291159214110107dad5e0ebea4b071c6ca9a8a7c
Rafael-Fonseca <rafael.abreu.unb@gmail.com>
2020-02-25 19:04:46 -0300

    Criação da tela principal
    Só isso por enquanto

---
+++ main.py
@@ -0,0 +1,13 @@
+from tkinter import *
+
+
+class Application():
+    def __init__(self, master=None):
+        pass
+
+
+root = Tk()
+Application(root)
+root.geometry('2000x700+0+0')
+
+root.mainloop()
